7126d59bdf8d642605e8038da2ad72970d9472aa
Bent J. Lorentzen <bent.lorentzen@nav.no>
2023-02-07 07:58:39 +0100

    Kommentert ut RestOperation, siden denne ikke er i bruk.

--- src/main/kotlin/no/nav/bidrag/samhandler/config/RestConfig.kt
+++ src/main/kotlin/no/nav/bidrag/samhandler/config/RestConfig.kt
@@ -4,16 +4,14 @@ import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.datatype.jsr310.deser.YearMonthDeserializer
 import com.fasterxml.jackson.module.kotlin.KotlinModule
-import no.nav.bidrag.commons.web.config.RestOperationsAzure
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Import
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
 import java.time.YearMonth
 import java.time.format.DateTimeFormatter
 
+// @Import(RestOperationsAzure::class)
 @Configuration
-@Import(RestOperationsAzure::class)
 class RestConfig {
 
     @Bean
