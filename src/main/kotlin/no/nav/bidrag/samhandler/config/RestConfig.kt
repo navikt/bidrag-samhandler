13a3966628b9d48ed32a6f29ac104fa79441cf13
Simen Høston <simen.naper.hoston@webstep.no>
2023-04-05 12:21:09 +0200

    Endrer objectMapper til å være lik den i bidrag-person

--- src/main/kotlin/no/nav/bidrag/samhandler/config/RestConfig.kt
+++ src/main/kotlin/no/nav/bidrag/samhandler/config/RestConfig.kt
@@ -1,11 +1,8 @@
 package no.nav.bidrag.samhandler.config
 
-import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.datatype.jsr310.deser.YearMonthDeserializer
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.time.YearMonth
@@ -15,17 +12,16 @@ import java.time.format.DateTimeFormatter
 class RestConfig {
 
     @Bean
-    fun jacksonObjectMapper(): ObjectMapper {
-        return ObjectMapper()
-            .registerKotlinModule()
-            .registerModule(
+    fun jackson2ObjectMapperBuilderCustomizer(): Jackson2ObjectMapperBuilderCustomizer =
+        Jackson2ObjectMapperBuilderCustomizer {
+            it.failOnUnknownProperties(false)
+            it.failOnEmptyBeans(false)
+            it.modulesToInstall(
                 JavaTimeModule()
                     .addDeserializer(
                         YearMonth::class.java,
-                        YearMonthDeserializer(DateTimeFormatter.ofPattern("u-MM"))
+                        YearMonthDeserializer(DateTimeFormatter.ofPattern("u-MM")) // Denne trengs for å parse år over 9999 riktig.
                     )
             )
-            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-    }
+        }
 }
