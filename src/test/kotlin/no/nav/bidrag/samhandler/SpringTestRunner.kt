fcbd98543111d566168a3a113afb67ab91aa171b
Bent J. Lorentzen <bent.lorentzen@nav.no>
2023-05-08 13:21:45 +0200

    Spring boot 3

--- src/test/kotlin/no/nav/bidrag/samhandler/SpringTestRunner.kt
+++ src/test/kotlin/no/nav/bidrag/samhandler/SpringTestRunner.kt
@@ -1,12 +1,12 @@
 package no.nav.bidrag.samhandler
 
 import com.github.tomakehurst.wiremock.WireMockServer
-import com.nimbusds.jwt.SignedJWT
+import com.nimbusds.jose.JOSEObjectType
 import no.nav.bidrag.commons.web.test.HttpHeaderTestRestTemplate
 import no.nav.security.mock.oauth2.MockOAuth2Server
+import no.nav.security.mock.oauth2.token.DefaultOAuth2TokenCallback
 import no.nav.security.token.support.spring.test.EnableMockOAuth2Server
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
@@ -15,15 +15,13 @@ import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock
 import org.springframework.context.ApplicationContext
 import org.springframework.http.HttpHeaders
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.web.util.UriComponentsBuilder
 
-@ExtendWith(SpringExtension::class)
-@ContextConfiguration(classes = [BidragSamhandlerLocal::class])
-@SpringBootTest(classes = [BidragSamhandlerLocal::class], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureWireMock(port = 0)
 @ActiveProfiles("test")
+// @ExtendWith(SpringExtension::class)
+// @ContextConfiguration(classes = [BidragSamhandlerLocal::class])
+@SpringBootTest(classes = [BidragSamhandlerLocal::class], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock
 @EnableMockOAuth2Server
 class SpringTestRunner {
 
@@ -67,8 +65,21 @@ class SpringTestRunner {
             add(HttpHeaders.AUTHORIZATION) { generateTestToken() }
         }
 
-    private fun generateTestToken(): String {
-        val token: SignedJWT = mockOAuth2Server.issueToken("aad", "aud-localhost", "aud-localhost")
+    fun generateTestToken(): String {
+        val iss = mockOAuth2Server.issuerUrl("aad")
+        val newIssuer = iss.newBuilder().host("localhost").build()
+        val token = mockOAuth2Server.issueToken(
+            "aad",
+            "aud-localhost",
+            DefaultOAuth2TokenCallback(
+                "aad",
+                "aud-localhost",
+                JOSEObjectType.JWT.type,
+                listOf("aud-localhost"),
+                mapOf("iss" to newIssuer.toString()),
+                3600
+            )
+        )
         return "Bearer " + token.serialize()
     }
 
