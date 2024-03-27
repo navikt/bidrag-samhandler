52c9b62a9c0c3a88dddd01a6f4a7c8dc42715afd
Simen Høston <simen.naper.hoston@webstep.no>
2024-03-27 13:38:11 +0100

    Splitter tss og samhandler controller

--- src/main/kotlin/no/nav/bidrag/samhandler/controller/SamhandlerController.kt
+++ src/main/kotlin/no/nav/bidrag/samhandler/controller/SamhandlerController.kt
@@ -3,40 +3,31 @@ package no.nav.bidrag.samhandler.controller
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import no.nav.bidrag.domene.ident.Ident
-import no.nav.bidrag.samhandler.service.TssService
+import no.nav.bidrag.samhandler.service.SamhandlerService
 import no.nav.bidrag.transport.samhandler.SamhandlerDto
 import no.nav.bidrag.transport.samhandler.SamhandlersøkeresultatDto
 import no.nav.bidrag.transport.samhandler.SøkSamhandlerQuery
 import no.nav.security.token.support.core.api.Protected
-import no.rtv.namespacetss.TssSamhandlerData
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @Protected
-class SamhandlerController(val tssService: TssService) {
+class SamhandlerController(
+    private val samhandlerService: SamhandlerService,
+) {
     @PostMapping("/samhandler")
     @Operation(
-        description = "Henter samhandler for ident",
+        description = "Henter samhandler for ident.",
         security = [SecurityRequirement(name = "bearer-key")],
     )
     fun hentSamhandler(
         @RequestBody ident: Ident,
     ): SamhandlerDto? {
-        return tssService.hentSamhandler(ident)
-    }
-
-    @PostMapping("/samhandlerData")
-    @Operation(
-        description = "Henter samhandler for ident og lever ut all data på identen slik det hentes fra TSS",
-        security = [SecurityRequirement(name = "bearer-key")],
-    )
-    fun hentSamhandlerData(
-        @RequestBody ident: Ident,
-    ): TssSamhandlerData {
-        return tssService.hentSamhandlerData(ident)
+        return samhandlerService.hentSamhandler(ident)
     }
 
     @GetMapping("/samhandler")
@@ -45,6 +36,28 @@ class SamhandlerController(val tssService: TssService) {
         security = [SecurityRequirement(name = "bearer-key")],
     )
     fun søkSamhandler(søkSamhandlerQuery: SøkSamhandlerQuery): SamhandlersøkeresultatDto {
-        return tssService.søkSamhandler(søkSamhandlerQuery)
+        return samhandlerService.søkSamhandler(søkSamhandlerQuery)
+    }
+
+    @PostMapping("/opprettSamhandler")
+    @Operation(
+        description = "Oppretter samhandler.",
+        security = [SecurityRequirement(name = "bearer-key")],
+    )
+    fun opprettSamhandler(
+        @RequestBody samhandlerDto: SamhandlerDto,
+    ) {
+        samhandlerService.opprettSamhandler(samhandlerDto)
+    }
+
+    @PutMapping("/samhandler")
+    @Operation(
+        description = "Oppdaterer samhandler.",
+        security = [SecurityRequirement(name = "bearer-key")],
+    )
+    fun oppdaterSamhandler(
+        @RequestBody samhandlerDto: SamhandlerDto,
+    ) {
+        samhandlerService.oppdaterSamhandler(samhandlerDto)
     }
 }
