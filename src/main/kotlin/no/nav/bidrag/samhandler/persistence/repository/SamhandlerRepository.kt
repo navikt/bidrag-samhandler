a8fcb9e866105a0ecce077718cf7a4a3852065e6
Simen Høston <simen.naper.hoston@webstep.no>
2024-03-27 13:37:30 +0100

    Legger til samhandler og repository

---
+++ src/main/kotlin/no/nav/bidrag/samhandler/persistence/repository/SamhandlerRepository.kt
@@ -0,0 +1,15 @@
+package no.nav.bidrag.samhandler.persistence.repository
+
+import no.nav.bidrag.samhandler.persistence.entity.Samhandler
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface SamhandlerRepository : JpaRepository<Samhandler, Int> {
+    fun findByIdent(ident: String): Samhandler?
+
+    fun findAllByNavnIgnoreCaseAndPostnr(
+        navn: String,
+        postnr: String?,
+    ): List<Samhandler>
+
+    fun findAllByNavnIgnoreCase(navn: String): List<Samhandler>
+}
