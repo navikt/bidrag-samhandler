daef0e1072009475f10c58e799ba35747e863d06
Simen Høston <simen.naper.hoston@webstep.no>
2024-04-02 14:21:37 +0200

    Nytt søk for opphenting av samhandlere med muligheter for søk på alle kontonummerfelter, navn, postnummer og poststed og alle kombinasjoner av disse

--- src/main/kotlin/no/nav/bidrag/samhandler/persistence/repository/SamhandlerRepository.kt
+++ src/main/kotlin/no/nav/bidrag/samhandler/persistence/repository/SamhandlerRepository.kt
@@ -2,8 +2,9 @@ package no.nav.bidrag.samhandler.persistence.repository
 
 import no.nav.bidrag.samhandler.persistence.entity.Samhandler
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 
-interface SamhandlerRepository : JpaRepository<Samhandler, Int> {
+interface SamhandlerRepository : JpaRepository<Samhandler, Int>, JpaSpecificationExecutor<Samhandler> {
     fun findByIdent(ident: String): Samhandler?
 
     fun findAllByNavnIgnoreCaseAndPostnr(
