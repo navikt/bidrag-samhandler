d3189a6f35bf3382abab4875c03dbc242d65bccd
Simen Høston <simen.naper.hoston@webstep.no>
2024-11-06 14:50:47 +0100

    Spesifierer json type på auditlog

--- src/main/kotlin/no/nav/bidrag/samhandler/persistence/entity/AuditLog.kt
+++ src/main/kotlin/no/nav/bidrag/samhandler/persistence/entity/AuditLog.kt
@@ -1,9 +1,11 @@
 package no.nav.bidrag.samhandler.persistence.entity
 
+import io.hypersistence.utils.hibernate.type.json.JsonType
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import org.hibernate.annotations.Type
 import java.time.LocalDateTime
 
 @Entity(name = "audit_log")
@@ -16,6 +18,8 @@ data class AuditLog(
     val operasjon: String = "",
     val endretTidspunkt: LocalDateTime = LocalDateTime.now(),
     val endretAv: String = "",
+    @Type(JsonType::class)
     val gamleVerdier: String = "",
+    @Type(JsonType::class)
     val nyeVerdier: String = "",
 )
