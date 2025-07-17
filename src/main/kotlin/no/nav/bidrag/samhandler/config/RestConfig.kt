b8fda97da2e88900ab6a769d77f7dbc8b27e58ab
Ugur Alpay Cenar <cenar93@gmail.com>
2025-07-17 20:07:38 +0200

    Visningsnavn

--- src/main/kotlin/no/nav/bidrag/samhandler/config/RestConfig.kt
+++ src/main/kotlin/no/nav/bidrag/samhandler/config/RestConfig.kt
@@ -2,6 +2,8 @@ package no.nav.bidrag.samhandler.config
 
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.datatype.jsr310.deser.YearMonthDeserializer
+import no.nav.bidrag.commons.service.KodeverkProvider
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -9,7 +11,13 @@ import java.time.YearMonth
 import java.time.format.DateTimeFormatter
 
 @Configuration
-class RestConfig {
+class RestConfig(
+    @Value("\${KODEVERK_URL}") kodeverkUrl: String,
+) {
+    init {
+        KodeverkProvider.initialiser(kodeverkUrl)
+    }
+
     @Bean
     fun jackson2ObjectMapperBuilderCustomizer(): Jackson2ObjectMapperBuilderCustomizer =
         Jackson2ObjectMapperBuilderCustomizer {
--- src/main/kotlin/no/nav/bidrag/samhandler/controller/VisningsnavnController.kt
+++ src/main/kotlin/no/nav/bidrag/samhandler/controller/VisningsnavnController.kt
@@ -1,5 +1,7 @@
 package no.nav.bidrag.samhandler.controller
 
+import no.nav.bidrag.commons.service.LANDKODER
+import no.nav.bidrag.commons.service.hentKodeverkKodeBeskrivelseMap
 import no.nav.bidrag.domene.enums.samhandler.Kreditortype
 import no.nav.bidrag.domene.enums.samhandler.OffentligIdType
 import no.nav.bidrag.domene.enums.samhandler.Valutakode
@@ -17,5 +19,6 @@ class VisningsnavnController {
     fun hentVisningsnavn(): Map<String, String> =
         OffentligIdType.entries.associate { it.name to it.visningsnavn.intern } +
             Kreditortype.entries.associate { it.name to it.visningsnavn } +
-            Valutakode.entries.associate { it.name to it.visningsnavn }
+            Valutakode.entries.associate { it.name to it.visningsnavn } +
+            hentKodeverkKodeBeskrivelseMap(LANDKODER).entries.associate { it.key to it.value }
 }
