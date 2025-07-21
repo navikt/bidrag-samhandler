705dfef0cf9cc7a05748e8642ae57a08cd8b04af
Simen Høston <simen.naper.hoston@webstep.no>
2025-07-21 12:27:23 +0200

    Endrer Visningsnavnkontroller til å bruke @RestController og legger til @Protected for sikkerhet

--- src/main/kotlin/no/nav/bidrag/samhandler/controller/Visningsnavnkontroller.kt
+++ src/main/kotlin/no/nav/bidrag/samhandler/controller/Visningsnavnkontroller.kt
@@ -2,10 +2,12 @@ package no.nav.bidrag.samhandler.controller
 
 import no.nav.bidrag.domene.enums.samhandler.OffentligIdType
 import no.nav.bidrag.domene.util.visningsnavn
-import org.springframework.stereotype.Controller
+import no.nav.security.token.support.core.api.Protected
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
+@Protected
 class Visningsnavnkontroller {
     @GetMapping("/visningsnavn")
     fun hentVisningsnavn(): Map<String, String> = OffentligIdType.entries.associate { it.name to it.visningsnavn.intern }
