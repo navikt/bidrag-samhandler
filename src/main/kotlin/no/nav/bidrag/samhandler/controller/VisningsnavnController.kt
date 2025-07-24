4a47b1c56454b3d2a0f3928330a69a45b259adea
Ugur Alpay Cenar <cenar93@gmail.com>
2025-07-24 18:00:07 +0200

    Legg til kodeverk i outbound rules og legg til flere visningsnavn (#150)

--- src/main/kotlin/no/nav/bidrag/samhandler/controller/VisningsnavnController.kt
+++ src/main/kotlin/no/nav/bidrag/samhandler/controller/VisningsnavnController.kt
@@ -2,10 +2,12 @@ package no.nav.bidrag.samhandler.controller
 
 import no.nav.bidrag.commons.service.LANDKODER
 import no.nav.bidrag.commons.service.hentKodeverkKodeBeskrivelseMap
+import no.nav.bidrag.domene.enums.diverse.Språk
 import no.nav.bidrag.domene.enums.samhandler.Kreditortype
 import no.nav.bidrag.domene.enums.samhandler.OffentligIdType
 import no.nav.bidrag.domene.enums.samhandler.Valutakode
 import no.nav.bidrag.domene.util.visningsnavn
+import no.nav.bidrag.transport.samhandler.Områdekode
 import no.nav.security.token.support.core.api.Protected
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
@@ -17,6 +19,8 @@ class VisningsnavnController {
     fun hentVisningsnavn(): Map<String, String> =
         OffentligIdType.entries.associate { it.name to it.visningsnavn.intern } +
             Kreditortype.entries.associate { it.name to it.visningsnavn } +
+            Områdekode.entries.associate { it.name to it.visningsnavn } +
             Valutakode.entries.associate { it.name to it.visningsnavn } +
+            Språk.entries.associate { it.name to it.visningsnavn } +
             hentKodeverkKodeBeskrivelseMap(LANDKODER).entries.associate { it.key to it.value }
 }
